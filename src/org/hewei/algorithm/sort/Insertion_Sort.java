ffe7dae687bc5e07253d38ab2fb9b2e8405c6043
DejaVu <MrQunma@outlook.com>
2016-12-29 01:07:59 +0800

    Insertion sort

---
+++ src/org/hewei/algorithm/sort/Insertion_Sort.java
@@ -0,0 +1,52 @@
+package org.hewei.algorithm.sort;
+
+
+import java.util.Scanner;
+
+public class Insertion_Sort {
+    public static void main(String[] args) {
+
+        final int MAX = 1000;
+
+        int n;
+        int[] R = new int[MAX];
+
+        Scanner input = new Scanner(System.in);
+
+        n = input.nextInt();
+        for(int i = 0; i < n; i++) { R[i] = input.nextInt(); }
+
+        insertionSort(R, n);
+
+    }
+
+    /**
+     * 插入排序
+     * @param R 数组
+     * @param n 元素个数
+     */
+    private static void insertionSort(int[] R, int n) {
+        for(int i = 1; i < n; i++) {
+            int v = R[i];
+            int j = i - 1;
+            while (j >= 0 && R[j] > v) {
+                R[j+1] = R[j];
+                j--;
+            }
+            R[j+1] = v;
+            printArray(R, n);
+        }
+    }
+
+    /**
+     * 打印数组
+     * @param R
+     * @param n
+     */
+    private static void printArray(int[] R, int n) {
+        for(int i = 0; i < n; ++i) {
+            System.out.print(R[i] + " ");
+        }
+        System.out.println();
+    }
+}
